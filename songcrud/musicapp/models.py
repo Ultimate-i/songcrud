bbc0308a2f55d8a2c3cf1eade726b08dd77abc20
Jude Odika <judeodika@gmail.com>
2022-10-27 18:36:38 +0100

    Add models

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.IntegerField()
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100000)
+    music_id = models.IntegerField()
