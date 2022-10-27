1ee41c0f3f742f9fd4258bf45f4824e18e411e11
Jude Odika <judeodika@gmail.com>
2022-10-27 19:23:40 +0100

    Update column headers

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -8,14 +8,21 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=40)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.first_name
+
 class Song(models.Model):
-    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=40)
     date_released = models.DateField(default=datetime.today)
     likes = models.IntegerField()
-    artiste_id = models.IntegerField()
+
+    def __str__(self):
+         return self.title
 
 class Lyric(models.Model):
-    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=100000)
-    music_id = models.IntegerField()
+
+    def __str__(self):
+        return self.content
